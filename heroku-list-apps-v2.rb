e7172629b41f2a2c580bc71e58606d7ec9d2cfe6
Khalid Shaikh <kshaikhr@salido.com>
2020-03-22 00:33:03 -0400

    Storing passwords in yaml file

--- heroku-list-apps-v2.rb
+++ heroku-list-apps-v2.rb
@@ -6,6 +6,19 @@ require 'yaml/store'
 # Select the type of database that the user wishes to generate the new configuration string for
 env = :staging or :production
 
+passwords = {}
+passwords[:production] = {}
+passwords[:staging] = {}
+passwords[:production][:salido] = ''
+passwords[:staging][:salido] = ''
+passwords[:production][:readonly] = ''
+passwords[:staging][:readonly] = ''
+
+store = YAML::Store.new 'passwords.yml'
+store.transaction do
+  passwords = store[:passwords]
+end
+
 store = YAML::Store.new 'data.yml'
 heroku_list_of_application_environment_variables = store.transaction do
   store[:heroku_list_of_application_environment_variables]
@@ -54,51 +67,44 @@ package.workbook.add_worksheet do |worksheet|
     # environment_variables = environment_variables_with_mongodb.map { |e| e.split(":").first }
     environment_variables = environment_variables_with_mongodb.inject({}){|a,b|v=b.split(':'); a[v.first.strip] = v[1..-1].join(':').strip;a}
 
-    # binding.pry
-    # if environment_variables.length > 0
-
-      # output = `heroku config:get -a #{application_name.split(" ").first} #{environment_variables.join(' ')}`
-      j = 0
-
-      environment_variables.keys.each do |name|
-        value = environment_variables[name]
-        username = nil
-        if value[/sally|itp-team|tim|platform|salido\:/]
-          username = 'salido'
-          password = 'DMNqhIEti96SqxUy' if env == :production
-          password = 'zjMu7GoNeYW3WZ8P' if env == :staging
-        end
-        if value[/readonly\:/]
-          username = 'readonly'
-          password = 'TuZLUp9jvuCuTAl4' if env == :production
-          password = 'AxLJs32P4R3zw8vG' if env == :staging
-        end
-
-        # RegEx to capture name of the database from the legacy string
-        value[/\d+\/(.*)\?/] or value[/net\/(.*)\?/]
-        database = $1
-        database = 'platform_staging' if database.empty?
-
-        server_name = "production-main" if value[/ds015978/]
-        server_name = "production-storage" if value[/ds125204/]
-        mongo_code = "yxjqz" if value[/ds015978|ds125204/]
-
-        if value[/sbx-stg|sandbox-staging/]
-          server_name = "sandbox-staging"
-          mongo_code = "jxu4y"
-        end
-
-        replica_set_name = server_name if[:staging, :production].include? env
-
-        connection_string = if application_name[/ss-geoffrey|ss-gandalf/]
-                              "mongodb+srv://#{username}:#{password}@#{server_name}-#{mongo_code}.mongodb.net/#{database}?retryWrites=true&w=majority"
-                            else
-                              "mongodb://#{username}:#{password}@#{server_name}-shard-00-00-#{mongo_code}.mongodb.net:27017,#{server_name}-shard-00-01-#{mongo_code}.mongodb.net:27017,#{server_name}-shard-00-02-#{mongo_code}.mongodb.net:27017/#{database}?ssl=true&replicaSet=#{replica_set_name}-shard-0&authSource=admin&retryWrites=true&w=majority"
-                            end
-
-        puts "heroku config:set -a #{application_name.split(" ").first} #{environment_variables[j]}=\"#{connection_string}\""
-        j += 1
+    environment_variables.keys.each do |name|
+      value = environment_variables[name]
+      username = nil
+      if value[/sally|itp-team|tim|platform|salido\:/]
+        username = 'salido'
+        password = 'DMNqhIEti96SqxUy' if env == :production
+        password = 'zjMu7GoNeYW3WZ8P' if env == :staging
       end
+      if value[/readonly\:/]
+        username = 'readonly'
+        password = 'TuZLUp9jvuCuTAl4' if env == :production
+        password = 'AxLJs32P4R3zw8vG' if env == :staging
+      end
+
+      # RegEx to capture name of the database from the legacy string
+      value[/\d+\/(.*)\?/] or value[/net\/(.*)\?/]
+      database = $1
+      database = 'platform_staging' if database.empty?
+
+      server_name = "production-main" if value[/ds015978/]
+      server_name = "production-storage" if value[/ds125204/]
+      mongo_code = "yxjqz" if value[/ds015978|ds125204/]
+
+      if value[/sbx-stg|sandbox-staging/]
+        server_name = "sandbox-staging"
+        mongo_code = "jxu4y"
+      end
+
+      replica_set_name = server_name if[:staging, :production].include? env
+
+      connection_string = if application_name[/ss-geoffrey|ss-gandalf/]
+                            "mongodb+srv://#{username}:#{password}@#{server_name}-#{mongo_code}.mongodb.net/#{database}?retryWrites=true&w=majority"
+                          else
+                            "mongodb://#{username}:#{password}@#{server_name}-shard-00-00-#{mongo_code}.mongodb.net:27017,#{server_name}-shard-00-01-#{mongo_code}.mongodb.net:27017,#{server_name}-shard-00-02-#{mongo_code}.mongodb.net:27017/#{database}?ssl=true&replicaSet=#{replica_set_name}-shard-0&authSource=admin&retryWrites=true&w=majority"
+                          end
+
+      puts "heroku config:set -a #{application_name.split(" ").first} #{name}=\"#{connection_string}\""
+    end
 
     row = worksheet.add_row [
                                 application_name,
